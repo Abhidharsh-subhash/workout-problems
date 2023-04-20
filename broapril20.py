3e7eb236a0c13cd1e1f7797b33ae40dba6205704
abhidharshss <abhidharsh1999@gmail.com>
2023-04-20 09:32:43 +0530

    brototype

---
+++ broapril20.py
@@ -0,0 +1,18 @@
+def common(array1,array2):
+    result=[]
+    for i in array1:
+        if i in array2:
+            result.append(i)
+    return result
+
+arr1=[]
+arr2=[]
+n1=int(input('number of elements in first array'))
+for i in range(n1):
+    el=int(input())
+    arr1.append(el)
+n2=int(input('number of elements in second array'))
+for i in range(n2):
+    el=int(input())
+    arr2.append(el)
+print(common(arr1,arr2))
