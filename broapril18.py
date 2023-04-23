ff0fb80c01e92a0734f05ef227682c900f48aee9
abhidharshss <abhidharsh1999@gmail.com>
2023-04-23 15:41:13 +0530

    second largest

--- broapril18.py
+++ broapril18.py
@@ -1,4 +1,17 @@
 #second largest element in an array
 
 def second(array):
-    largest=
+    if len(array)>1:
+        a=array[0]
+        b=array[1]
+        for i in range(2,len(array)):
+            if array[i] > a:
+                b=a
+                a=array[i]
+            elif array[i] > b:
+                b=array[i]
+        return b
+
+arr=[44,6,88,43,78,67,99]
+print(second(arr))
+    
