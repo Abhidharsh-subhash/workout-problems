36e7f1e89a72ab6f84be841505d754f05bb9ab79
abhidharshss <abhidharsh1999@gmail.com>
2023-05-15 11:10:53 +0530

    2586

---
+++ 2586.py
@@ -0,0 +1,32 @@
+# Input: words = ["are","amy","u"], left = 0, right = 2
+# Output: 2
+# Explanation: 
+# - "are" is a vowel string because it starts with 'a' and ends with 'e'.
+# - "amy" is not a vowel string because it does not end with a vowel.
+# - "u" is a vowel string because it starts with 'u' and ends with 'u'.
+# The number of vowel strings in the mentioned range is 2.
+# Example 2:
+
+# Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+# Output: 3
+# Explanation: 
+# - "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+# - "mu" is not a vowel string because it does not start with a vowel.
+# - "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+# - "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+# The number of vowel strings in the mentioned range is 3.
+
+def vowelstrings(words):
+    vowels=['a', 'e', 'i', 'o','u']
+    count=0
+    for i in words:
+        if len(i) == 1:
+            if i in vowels:
+                count+=1
+        else:
+            if (i[0] in vowels) and (i[-1] in vowels):
+                count+=1
+    return count
+
+words=["vo","j","i","s","i"]
+print(vowelstrings(words))
