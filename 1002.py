ada6ecd56a65087bd915a8b116ec35a8604c40f2
abhidharshss <abhidharsh1999@gmail.com>
2023-05-16 14:45:42 +0530

    1002leetcode

--- 1002.py
+++ 1002.py
@@ -1,5 +1,3 @@
-#incomplete
-
 # Example 1:
 # Input: words = ["bella","label","roller"]
 # Output: ["e","l","l"]
@@ -7,15 +5,37 @@
 # Input: words = ["cool","lock","cook"]
 # Output: ["c","o"]
 
-def repeating(array):
-    result=[]
-    for i in array:
-        for j in i:
-            if (j in array[1]) and (j in array[2]):
-                if j not in result:
-                    result.append(j) 
-        break
-    return result
+def repeating(A):
+    # result = []
+    # for c in set(A[0]):
+    #         count = A[0].count(c)
+    #         occurences = 1
+    #         for i in range(1,len(A)):
+    #             if c in A[i]:
+    #                 count = min(count,A[i].count(c))
+    #                 occurences += 1
+    #             else:
+    #                 break
+    #         if occurences == len(A):
+    #             for i in range(count):
+    #                 result.append(c)
+                    
+    # return result
+    count = {}
+    for x in A[0]:
+        count[x] = count.get(x, 0) + 1
+            
+    for x in count:
+        for i in range(1,len(A)):
+            if x in A[i]:
+                count[x] = min(count[x], A[i].count(x))
+            else:
+                count[x] = 0
+                break 
+    ans = []
+    for k,v in count.items():
+            ans += v * [k]
+    return ans
 
 words = ["cool","lock","cook"]
 print(repeating(words))
