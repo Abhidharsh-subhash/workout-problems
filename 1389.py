70a0010abac2ac84e31a45c4aa7f0d6664dcd383
abhidharshss <abhidharsh1999@gmail.com>
2023-07-24 19:53:29 +0530

    ntg

--- 1389.py
+++ 1389.py
@@ -8,22 +8,24 @@
 # 3            2        [0,1,3,2]
 # 4            1        [0,4,1,3,2]
 
-def createTargetArray(nums,index):
-    result=[None]*len(nums)
-    for i in range(len(nums)):
-        x=index[i]
-        y=nums[x]
-        if result[x] is None:
-            result[x]=y
-        else:
-            for j in range(i-1,-1,-1):
-                print(result[j])
-                result[j+1]=result[j]
-            x=index[i]
-            y=nums[x]
-            result[x]=y
-    print(result)
+# def createTargetArray(nums,index):
+#     result=[None]*len(nums)
+#     for i in range(len(nums)):
+#         x=index[i]
+#         y=nums[x]
+#         if result[x] is None:
+#             result[x]=y
+#         else:
+#             result[x]=y
+#     print(result)
 
-nums = [0,1,2,3,4]
-index = [0,1,2,2,1]
-print(createTargetArray(nums,index))
+# nums = [0,1,2,3,4]
+# index = [0,1,2,2,1]
+# print(createTargetArray(nums,index))
+
+x=[]
+y=[1,2,3,4]
+for i in y:
+    x+=[i]
+print(x)
+abhidharsh
