2e861b272c9331812910a7e0332f58d5e6cc8403
abhidharshss <abhidharsh1999@gmail.com>
2023-07-30 19:10:06 +0530

    1

---
+++ 2710.py
@@ -0,0 +1,12 @@
+# Input: num = "51230100"
+# Output: "512301"
+# Explanation: Integer "51230100" has 2 trailing zeros, we remove them and return integer "512301".
+
+def removeTrailingZeros(num):
+    array=[x for x in num]
+    print(array)
+    for i in array:
+        
+
+num = "51230100"
+print(removeTrailingZeros(num))
